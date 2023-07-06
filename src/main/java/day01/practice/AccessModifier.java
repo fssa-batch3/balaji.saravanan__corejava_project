5b628a5f8f0232a916ef436c04e9ea565fa8b2ce
Balajis007 <donbalajis007@gmail.com>
2023-07-06 11:27:10 +0530

    practice

---
+++ src/main/java/day01/practice/AccessModifier.java
@@ -0,0 +1,15 @@
+package day01.practice;
+
+public class AccessModifier {
+	static String a = "a";
+	static String b = "b";
+	static String c = "c";
+	static String d = "d";
+
+	public static void main(String[] args) {
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
